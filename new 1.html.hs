2bf60ecc4609464c848770c892d1361e0cd6e428
Manish kumar gupta <introhacking@gmail.com>
2022-11-22 13:00:46 +0530

    cascaron first commit

---
+++ new 1.html.hs
@@ -0,0 +1,41 @@
+<section class="section-padding-100-70 clearfix relative cir-left">
+        
+        <div class="container">
+            <div class="row align-items-center">
+                <div class="col-12 col-lg-6 offset-lg-0 col-md-12 no-padding-left">
+                    <img class="img-responsive" src="img/core-img/about3.png" alt="">
+                </div>
+                
+                <div class="col-12 col-lg-6 offset-lg-0">
+                    <div class="who-we-contant mt-s">
+                        <div class="dream-dots text-left fadeInUp" data-wow-delay="0.2s">
+                            <span class="gradient-text blue">New problems need Creative and unusual Solutions</span>
+                        </div>
+                        <h4 class="fadeInUp" data-wow-delay="0.3s">Apply AI, Deep Learning and Data Sciece to solve Business Problems</h4>
+                        <p class="fadeInUp" data-wow-delay="0.4s">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Suscipit eum illum tempora? Ducimus eum culpa voluptates dolorem dolorum et sit nisi, mollitia animi porro fuga sequi, molestias repellat excepturi nobis eum culpa voluptates dolorem dolorum et.</p>
+                        <div class="services-block-four v2 mt-30">
+                            <div class="inner-box">
+                                <div class="icon-font-box">
+                                    <i class="fa fa-check"></i>
+                                </div>
+                                <h3><a href="#">Powerfull Mobile And Online App</a></h3>
+                                <div class="text width-80">Lorem ipsum dolor sit amet, conse ctetur dolor adipisicing elit alias officia aperiam.</div>
+                            </div>
+                        </div>
+                        <div class="services-block-four v2">
+                            <div class="inner-box">
+                                <div class="icon-font-box">
+                                    <i class="fa fa-check"></i>
+                                </div>
+                                <h3><a href="#">Special For Multiple Use Capabilities</a></h3>
+                                <div class="text width-80">Lorem ipsum dolor sit amet, conse ctetur dolor adipisicing elit alias officia aperiam.</div>
+                            </div>
+                        </div>
+
+
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </section>
